a2e62c9b762c21410b046ff83cb6f8ffad74b717
tdc <tdc@tdccc.com.tw>
2020-01-10 14:33:17 +0800

    新增總金額 && fix error

--- main.py
+++ main.py
@@ -60,6 +60,23 @@ class bot:
                 pass
             else:
                 return
+
+            # total price
+            url = 'https://mbb5g.ncc.gov.tw/index01.html'
+            r = requests.get(url)
+            r.encoding = 'utf8'
+            logger.info('Load Total Price')
+            if r.status_code != 200:
+                logger.critical('load total price failed')
+                return False
+
+            total = '-'
+            try:
+                total = list(pq(r.text)('.content')('.unit').items())[
+                    0].text().split('總標金：')[1]
+            except Exception as e:
+                logger.critical(e)
+
             text = f'\[{data[0]}]\n\n' + \
                 '*1800MHz*\n' + \
                 f'回合價：`{data[1]}`\n' + \
@@ -75,6 +92,7 @@ class bot:
                 f'回合價暫得標頻寬：`{data[9]}`\n' + \
                 f'暫時得標頻寬：`{data[10]}`\n' + \
                 f'暫時得標金：`{data[11]}`\n\n' + \
+                f'總標金：`{total}``' + \
                 f'暫時得標金單位：新臺幣百萬元'
             self.bot.send_message(config.channel, text, parse_mode='markdown')
 
@@ -98,7 +116,7 @@ class bot:
         text = f'今日 {now} 競標結束\n' +\
                f'剩餘合格競標廠商：`{company}`\n\n' +\
             '#競標結束'
-        self.bot.send_message(channel, text, parse_mode='markdown')
+        self.bot.send_message(config.channel, text, parse_mode='markdown')
 
 
 notify = bot()
